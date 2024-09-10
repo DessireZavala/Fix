6ec99b644c6d66b768960ff087cff0660802800e
LuisDani <spartandanii117@gmail.com>
2024-09-10 00:04:23 -0600

    Primer caambio

--- config/filesystems.php
+++ config/filesystems.php
@@ -29,12 +29,12 @@ return [
     */
 
     'disks' => [
-
-        'local' => [
-            'driver' => 'local',
-            'root' => storage_path('app'),
-            'throw' => false,
-        ],
+    'public' => [
+        'driver' => 'local',
+        'root' => public_path('storage'),
+        'url' => env('APP_URL').'/storage',
+        'visibility' => 'public',
+    ],
 
         'public' => [
             'driver' => 'local',
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GoogleController;
+use App\Http\Controllers\PostController;
 
 
 Route::get('/', function () {
@@ -17,6 +18,8 @@ Route::middleware('auth', 'verified')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dashboard', [PostController::class, 'index'])->name('dashboard');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 
     
 });
